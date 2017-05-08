dbed6ec66e772e8eea644a2a1db73d4fda7d405c
Kolandrious <glisterminsk@gmail.com>
2017-05-08 19:33:28 +0300

    flexbox styles

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 export default props => {
   return (
-    <div className="screen container col-sm-9 align-items-center">
-      <div className="row justify-content-center" >Tell me whats wrong (pick one)</div>
-      <div className="row" ><button className="category" onClick={() => {props.history.push('sore-throat')}}>Sore Throat</button></div>
-      <div className="row" ><button className="category ">Fever</button></div>
-      <div className="row" ><button className="category">Stress</button></div>
-      <div className="row" ><button className="category">Stomachache</button></div>
-      <div className="row" ><button className="category">Joint Pain</button></div>
+    <div className="screen container">
+      <div>Tell me whats wrong (pick one)</div>
+      <div className="items">
+        <div className="item"><button className="button" onClick={() => {props.history.push('sore-throat')}}>Sore Throat</button></div>
+        <div className="item"><button className="button">Fever</button></div>
+        <div className="item"><button className="button">Stress</button></div>
+        <div className="item"><button className="button">Stomachache</button></div>
+        <div className="item"><button className="button">Joint Pain</button></div>
+      </div>
       first screen
     </div>
   )
