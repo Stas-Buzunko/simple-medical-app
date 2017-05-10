33b07da2471ee98bd0313270b783b592e8bd2d42
Stas-Buzunko <buzunkostas@gmail.com>
2017-05-10 08:06:23 +0300

    fix styling a bit

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -9,16 +9,14 @@ export default (props) => {
 			      <div style={{height: '10%', marginTop: '25px'}}>
 			      	<p className="text-primary" style={{fontSize: '36px', color:'#00cccc'}}> Welcome to </p> 
 			      </div>
-			      <div style={{height: '30%', marginTop: '50px', }}>
+			      <div style={{height: '30%'}}>
 			      	<p className="text-primary" style={{fontSize: '86px', textDecoration: 'underline', textDecorationColor: '#337ab7'}}>Fix Me</p>
 			      </div>
-			      <div style={{height: '60%', width: '80%', margin: 'auto'}}>
-			        <div className="btn-container"><Button bsStyle="primary" bsSize="large" onClick={() => {props.changePage()}}>Continue to login to your account</Button>
-			        </div>
+			      <div style={{height: '60%', width: '100%', margin: 'auto'}}>
+			        <Button bsStyle="primary" bsSize="large" onClick={() => {props.changePage()}} style={{height: '20%'}}>Continue to login to your account</Button>
 			      </div>
 			    </div>
 	  	: 
-				
 			    <div className="col-xs-12 col-sm-12 col-md-4 center-block" style={{height: '100vh'}}>
 			      <div style={{height: '7%'}}>
 			      	<p className="text-primary" style={{fontSize: '26px'}}> Tell me whats wrong </p> 
@@ -33,6 +31,6 @@ export default (props) => {
 			      </div>
 			    </div>
 			}
-		 </div>
+		</div>
   )
 }
